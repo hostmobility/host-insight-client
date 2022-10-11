d6e3ba3d607c289f5222efe4b5dbd6eefe25fa25
Albin Söderqvist <albin.soderqvist@hostmobility.com>
2022-10-11 11:17:39 +0200

    feat: Update proto build process to handle optional values
    This is a temporary measure until prost can handle the new defaults of
    proto3. (Optional values are no longer experimental upstream.)

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/elevator.proto")?;
+    let mut config = prost_build::Config::new();
+    config.protoc_arg("--experimental_allow_proto3_optional");
+    tonic_build::configure().compile_with_config(config, &["proto/elevator.proto"], &["proto"])?;
     Ok(())
 }
