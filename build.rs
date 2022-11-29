9be3a5c078b178cbb8cc0a7accda03245b0b0b2f
Albin Söderqvist <albin.soderqvist@hostmobility.com>
2022-11-29 12:49:50 +0100

    Update proto and perform renames elevator -> ada

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Include the output of git describe --tags --long --dirty
     let git_describe_output = Command::new("git")
-        .args(&["describe", "--tags", "--long", "--dirty"])
+        .args(["describe", "--tags", "--long", "--dirty"])
         .output()
         .unwrap();
     let git_version = String::from_utf8(git_describe_output.stdout).unwrap();
@@ -11,6 +11,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Build proto
     let mut config = prost_build::Config::new();
     config.protoc_arg("--experimental_allow_proto3_optional");
-    tonic_build::configure().compile_with_config(config, &["proto/elevator.proto"], &["proto"])?;
+    tonic_build::configure().compile_with_config(
+        config,
+        &[
+            "proto/ada.proto",
+            "proto/ada_controller.proto",
+            "proto/ada_enums.proto",
+        ],
+        &["proto"],
+    )?;
     Ok(())
 }
